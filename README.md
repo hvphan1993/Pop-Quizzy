16f3767dc900dc46a4f2b49279071078849f4db7
hvphan <hvphan1993@gmail.com>
2022-04-06 21:01:31 -0700

    added timer feature, added sections to readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Pop-Quizzy
+
+## General Info
+
+
+## Creating the Timer
+
+
+## Setting Up High Score Storage
+
+
+## Process of Setting Up Quiz Responsive Questions
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,28 @@
 // eventlisteners for on click events, array to hold answers for localStorage, stored questions to reference in JSON, 
 // -- high score feature, timer feature, way to check accuracy of answer, add initials to high score
 
-function timer() {
-    var sec = 60
-}
+var timerEl = document.querySelector("#countdown");
+
+function countdown() {
+    var timeLeft = 60;
+      var timeInterval = setInterval(function () {
+     // write down count value when time is > than 1 (as count is going down)
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } 
+      // change the string when the timer is at one to be grammatically correct
+      else if (timeLeft === 1) {
+        timerEl.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      } 
+      // when timer has run out (less than 1) clear the interval and stop the quiz and run high score checker
+      else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+       // ;
+      }
+    }, 1000);
+  }
+
+  countdown();
