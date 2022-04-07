5b51120ca19147289ac8fc2f238c0a1c11548db6
hvphan <hvphan1993@gmail.com>
2022-04-06 19:03:16 -0700

    added basic html, css, and javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// needs for Sat: create 5 Qs and As, read about how to 
+// eventlisteners for on click events, array to hold answers for localStorage, stored questions to reference in JSON, 
+// -- high score feature, timer feature, way to check accuracy of answer, add initials to high score
+
+function timer() {
+    var sec = 60
+}
